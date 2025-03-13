7f25d9771e6837b4c9acf211a0305f65fd4462bd
aaronperkel <aaron.perkel27@gmail.com>
2025-03-13 08:58:24 -0400

    merged nav and header

--- src/nav.php
+++ src/nav.php
@@ -1,18 +1,14 @@
 <!-- nav.php -->
-<header>
+<nav class="main-nav">
+  <div class="nav-left">
     <h1>UVM Sublets</h1>
-</header>
-
-<?php
-$user = $_SERVER['REMOTE_USER'] ?? 'aperkel';
-?>
-
-
-<nav>
-  <div class="nav-links">
-    <a class="<?php if ($pathParts['filename'] == 'index') { print 'activePage'; } ?>" href="index.php">Home</a>
-    <a class="<?php if ($pathParts['filename'] == 'map') { print 'activePage'; } ?>" href="map.php">Map</a>
-    <a class="<?php if ($pathParts['filename'] == 'new_post') { print 'activePage'; } ?>" href="new_post.php">New Post</a>
   </div>
-  <p class="nav-user">Hello, <?php print $user; ?></p>
+  <div class="nav-right">
+    <div class="nav-links">
+      <a class="<?php if ($pathParts['filename'] == 'index') { print 'activePage'; } ?>" href="index.php">Home</a>
+      <a class="<?php if ($pathParts['filename'] == 'map') { print 'activePage'; } ?>" href="map.php">Map</a>
+      <a class="<?php if ($pathParts['filename'] == 'new_post') { print 'activePage'; } ?>" href="new_post.php">New Post</a>
+    </div>
+    <p class="nav-user">Hello, <?php print $_SERVER['REMOTE_USER'] ?? 'Guest'; ?></p>
+  </div>
 </nav>
