32826733123e9863db3c4611e5ad6a7d1807834f
aaronperkel <aaron.perkel27@gmail.com>
2025-03-29 12:15:20 -0400

    removed tip

--- src/nav.php
+++ src/nav.php
@@ -35,6 +35,11 @@ if (isset($_SERVER['REMOTE_USER'])) {
   </div>
 </nav>
 
-<div style="background-color: #dbe9f9; color: #4a90e2; border: 1px solid #b7d3f3; border-radius: 4px; padding: 10px; margin-bottom: 15px;">
-  Adding multiple photos is coming soon! Thank you for using my site! -AP
-</div>
+
+<!-- <div style="background-color: #f8d7da; color: #721c24; border: 1px solid #f5c6cb; padding: 10px; margin-bottom: 15px;">
+  WARNING
+</div> -->
+
+<!-- <div style="background-color: #dbe9f9; color: #4a90e2; border: 1px solid #b7d3f3; border-radius: 4px; padding: 10px; margin-bottom: 15px;">
+  INFO
+</div> -->
